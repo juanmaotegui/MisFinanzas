9bb2d06712f8ef363743955db38d5873b5886e19
Juan Manuel Otegui <juanma.otegui@gmail.com>
2021-02-07 11:59:59 -0300

    Agregado navigation, reanimated, gesture-handler

--- App.js
+++ App.js
@@ -5,22 +5,22 @@
  * @format
  * @flow strict-local
  */
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import Incomes from './src/screens/Incomes';
 import Expenses from './src/screens/Expenses';
 
 const App: () => React$Node = () => {
   return (
     <>
-      {/* <NavigationContainer> */}
-      <StatusBar backgroundColor="#949494" />
-      <SafeAreaView style={{ flex: 1 }}>
-        <Incomes />
-      </SafeAreaView>
-      {/* </NavigationContainer> */}
+      <NavigationContainer>
+        <StatusBar backgroundColor="#949494" />
+        <SafeAreaView style={{ flex: 1 }}>
+          <Incomes />
+        </SafeAreaView>
+      </NavigationContainer>
     </>
   );
 };
